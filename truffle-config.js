ad5dbcf6951d3d78ce8e2a489d2228a6d77de9a3
Ricardo C <ricardo.chavarria@tradeshift.com>
2019-03-28 15:13:48 +0100

    Match network id with value in genesis file

--- truffle-config.js
+++ truffle-config.js
@@ -39,7 +39,7 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '4242' // Match any network id
+      network_id: '2428' // Match any network id
     }
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
